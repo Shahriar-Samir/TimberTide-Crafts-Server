adb729bc61999311a930b6c39d7b5b9fb5863681
Shahriar-Samir <shabusiness035@gmail.com>
2024-04-26 14:05:51 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const cors = require('cors')
+
+const port = process.env.PORT || 5000
+const app = express()
+
+
+
+
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`)
+})
