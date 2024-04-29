59b9fd4d51d4f9220446cc798a50f61fe9425b15
Shahriar-Samir <shabusiness035@gmail.com>
2024-04-29 22:22:33 +0600

    commented some functions from client object

--- index.js
+++ index.js
@@ -30,6 +30,13 @@ async function run() {
   try {
 
 
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
     const craftsCollection = client.db('Art_and_craft_store').collection('arts&crafts')
     const subcategoriesCollection = client.db('Art_and_craft_store').collection('subcategories')
 
@@ -116,11 +123,9 @@ async function run() {
       res.send(deleteItem)
     })
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
+        // Ensures that the client will close when you finish/error
+        // await client.close();
   }
 }
 run().catch(console.dir);
